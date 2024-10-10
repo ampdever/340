078f31f13a5250b6f875cee24f102e403adc3293
ampdever <Mag23019@byui.edu>
2024-10-10 01:31:46 -0600

    w4 learning

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,8 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  // This next line is for the flash message on the home page - testing
+  //req.flash("notice", "This is a flash message.")
   res.render("index", {title: "Home", nav})
 }
 
